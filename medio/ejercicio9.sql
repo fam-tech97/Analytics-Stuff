459d97ac917d9003af2e2b26b8b9f3764920f478
Steven Arteaga <stevenfam1997@gmail.com>
2026-05-10 20:11:40 +0200

    Resubida del ejercicio 9

--- medio/ejercicio9.sql
+++ medio/ejercicio9.sql
@@ -1,20 +1,6 @@
-SELECT 
-    inspection_type,
-    [no_risks_results],
-    [Low Risk],
-    [Moderate Risk],
-    [High Risk],
-    ([no_risks_results] + [Low Risk] + [Moderate Risk] + [High Risk]) AS total_inspections
-FROM
-(
-    SELECT
-        inspection_type,
-        COALESCE(risk_category, 'no_risks_results') as risk_category,
-        inspection_id
-    FROM sf_restaurant_health_violations
-) AS source_table
-PIVOT
-(
-    COUNT(inspection_id)
-    FOR risk_category IN ([no_risks_results], [Low Risk], [Moderate Risk], [High Risk])
-) AS pivot_table
+select 
+100.0 * sum(
+    Case when c.address is not null or c.address <> '' then 1 else 0 end
+)/ count(distinct o.id)  as percent_shipable
+from orders O
+left join customers C on O.cust_id = C.id

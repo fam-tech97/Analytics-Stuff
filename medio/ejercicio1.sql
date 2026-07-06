444e90f481d92c15f23b8df7ca94e256c6fd10e0
Steven Arteaga <stevenfam1997@gmail.com>
2026-07-06 11:06:07 +0200

    Repaso ejercicio 1 users by avg session time

--- medio/ejercicio1.sql
+++ medio/ejercicio1.sql
@@ -1,35 +1,38 @@
-with page_loads as (
+--Crear una CTE tanto para page_loads como page_exits
+
+with loads as (
     select
         user_id,
-        cast(timestamp as date) as fecha,
-        max(timestamp) as page_load
+        cast(timestamp as DATE) as day,
+        max(timestamp) as load_time
     from facebook_web_log
     where action = 'page_load'
-    group by user_id, cast(timestamp as date)
+    group by user_id, cast(timestamp as DATE)
 ),
 
-page_exits as (
+exits as (
     select
         user_id,
-        cast(timestamp as date) as fecha,
-        min(timestamp) as page_exit
+        cast(timestamp as DATE) as day,
+        min(timestamp) as exit_time
     from facebook_web_log
     where action = 'page_exit'
-    group by user_id, cast(timestamp as date)
+    group by user_id, cast(timestamp as DATE)
 ),
-
-clear_data as (
-    select 
-        l.user_id,
-        l.fecha,
-        datediff(second, l.page_load, e.page_exit) as difference
-    from page_exits E
-    inner join page_loads L on L.user_id = E.user_id and L.fecha = E.fecha
-    where L.page_load < E.page_exit
+    
+diff as (    
+    select
+        L.user_id as user_id,
+        L.load_time as load_time,
+        E.exit_time as exit_time,
+        datediff(second, L.load_time, E.exit_time) as time_difference
+    from loads L
+    inner join exits E on E.user_id = L.user_id and E.day = L.day
+    where L.load_time < E.exit_time
 )
 
-select 
+select
 user_id,
-avg(difference * 1.0) as avg_session_duration
-from clear_data
+avg(1.0 * time_difference) as avg_session_duration
+from diff
 group by user_id

5903425dfdd12a4d779ec2044275e5c14194b65f
Steven Arteaga <stevenfam1997@gmail.com>
2026-08-22 23:24:56 +0200

    Repaso ejercicio 5 SQL. Vuelta a la rutina

--- medio/ejercicio5.sql
+++ medio/ejercicio5.sql
@@ -1,11 +1,13 @@
-with transactions as(
+with previous_dates as (
     select
         user_id,
-        cast(created_at as date) as fecha,
-        lag(cast(created_at as date)) OVER (PARTITION BY user_id order by created_at) as fecha_previa
+        cast(created_at as date) as primera_fecha,
+        lag(cast(created_at as date)) over (partition by user_id order by cast(created_at as date)) as fecha_previa
     from amazon_transactions
+    where revenue > 0
 )
 
-select distinct user_id
-from transactions
-where datediff(day, fecha, fecha_previa) < 0 and datediff(day, fecha, fecha_previa) >= -7
+select distinct
+    user_id
+from previous_dates
+where datediff(day, primera_fecha, fecha_previa) < 0 and datediff(day, primera_fecha, fecha_previa) >= -7

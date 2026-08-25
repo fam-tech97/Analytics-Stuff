3efd057a58c42ba606dcc31d0625661b1b237b7d
Steven Arteaga <stevenfam1997@gmail.com>
2026-08-25 17:37:42 +0200

    Repaso ejercicio 8 SQL. Vuelta a la rutina

--- medio/ejercicio8.sql
+++ medio/ejercicio8.sql
@@ -1,20 +1,24 @@
-SELECT 
+with data as (
+    select
+    inspection_id,
     inspection_type,
-    [no_risks_results],
-    [Low Risk],
-    [Moderate Risk],
-    [High Risk],
-    ([no_risks_results] + [Low Risk] + [Moderate Risk] + [High Risk]) AS total_inspections
-FROM
-(
-    SELECT
-        inspection_type,
-        COALESCE(risk_category, 'no_risks_results') as risk_category,
-        inspection_id
-    FROM sf_restaurant_health_violations
-) AS source_table
-PIVOT
-(
-    COUNT(inspection_id)
-    FOR risk_category IN ([no_risks_results], [Low Risk], [Moderate Risk], [High Risk])
-) AS pivot_table
+    max(case 
+        when risk_category = 'Low Risk' then 1
+        when risk_category = 'Moderate Risk' then 2
+        when risk_category = 'High Risk' then 3
+        else 0
+    end) as cases
+    from sf_restaurant_health_violations
+    group by inspection_id, inspection_type
+)
+
+
+select
+inspection_type,
+count(case when cases = 0 then 1 end) as no_risk_results,
+count(case when cases = 1 then 1 end) as low_risk_results,
+count(case when cases = 2 then 1 end) as medium_risk_results,
+count(case when cases = 3 then 1 end) as high_risk_results,
+count(*) as total_inspections
+from data
+group by inspection_type

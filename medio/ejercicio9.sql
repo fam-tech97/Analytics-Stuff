1f5f538f0420833cfeb66fb866e9a8c8c72a6cab
Steven Arteaga <stevenfam1997@gmail.com>
2026-08-26 12:43:48 +0200

    Repaso ejercicio 9 SQL. Vuelta a la rutina

--- medio/ejercicio9.sql
+++ medio/ejercicio9.sql
@@ -1,6 +1,12 @@
+with shipable as (
+    select
+        O.id as OrderId
+    from orders O
+    inner join customers C on O.cust_id = C.id
+    where C.address is not null or C.address <> ''
+)
+
 select 
-100.0 * sum(
-    Case when c.address is not null or c.address <> '' then 1 else 0 end
-)/ count(distinct o.id)  as percent_shipable
-from orders O
-left join customers C on O.cust_id = C.id
+100 * count(S.OrderId) / cast(count(O.id) as decimal) as percent_shipable
+from Orders O
+left join shipable S on S.OrderId = O.id
